b9ca299354988842f960eb70c52fc2629061990a
tinvo1101 <tinnvo1101@gmail.com>
2016-03-08 11:09:35 -0600

    Added ng-model to the checkboxes and initialized them to True

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,5 +2,10 @@ var app = angular.module('agentlistApp', []);
 
 app.controller('mainCtrl', function($scope, $http) {
 
+	$scope.addAgent = function(req, res) {
+		var  h  = $scope.req;
+		var a = JSON.stringify(h);
+		console.log(a);
+	};
 
 });
